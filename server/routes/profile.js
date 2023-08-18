a7c19a36e42b1cbe5528984733094104164c6e56
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-18 21:41:17 +0530

    cloudinary route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -55,4 +55,33 @@ router.put('/edit/:userId', async (req, res) => {
     }
 });
 
+
+//UPLOAD IMAGE USING CLOUDINARY
+// upload image
+router.post("/uploads/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    const { image } = req.body;
+    try {
+        if (image) {
+            const uploadRes = await cloudinary.uploader.upload(image, {
+                upload_preset: "user-profile"
+            });
+            if (uploadRes) {
+                user.image = uploadRes;
+                await user.save();
+                return res.status(200).json({ message: "Image uploaded successfully" });
+            }
+        } else {
+            return res.status(400).json({ message: "No image provided" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "An error occurred" });
+    }
+})
+
 module.exports = router;
