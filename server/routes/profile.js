21fb02f80b20128884e03232387782cf5cd778c8
Mahikagarg09 <mahika_garg@yahoo.com>
2023-08-18 15:50:19 +0530

    edit info of user route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users'); // Assuming your User model file is in this path
+const User = require('../models/users'); 
+const cloudinary = require("./cloudinary");
 
 // GET route to fetch all user information
 router.get('/:userId', async (req, res) => {
@@ -18,6 +19,39 @@ router.get('/:userId', async (req, res) => {
         res.status(200).json({ user });
     } catch (error) {
         res.status(500).json({ message: 'Error fetching user data' });
+    }
+});
+
+
+//EDIT ROUTE SO THAT USER CAN EDIT HIS INFORMATION
+router.put('/edit/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const updatedData = req.body; // The updated information sent in the request body
+
+    try {
+        // Find the user by their ID
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Update user properties based on the updatedData
+        if (updatedData.name)user.name = updatedData.name
+        if (updatedData.phone)user.phone = updatedData.phone;
+        if (updatedData.email)user.email = updatedData.email;
+        if (updatedData.password) user.password = updatedData.password;
+        if (updatedData.about) user.about = updatedData.about;
+        if (updatedData.skills) user.skills = updatedData.skills;
+        if (updatedData.certifications)user.certifications = updatedData.certifications;
+        if (updatedData.experience)user.experience = updatedData.experience;
+        if (updatedData.education)user.education = updatedData.education;
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({ message: 'User information updated', user: updatedUser });
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred', error: error.message });
     }
 });
 
